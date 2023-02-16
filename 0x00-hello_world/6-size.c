8443bc5fc5d41e12d81ad454005bc51807d8bea2
Xaldovah <dxaldovah@gmail.com>
2023-02-16 13:58:44 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Program that writes size of various types on the computer it is compiled and run on
- * Return: 0 (Let's go!)
+ * main - Program that writes size of various types on the computer
+ * Return: 0 (Success)
  */
 
 int main(void)
