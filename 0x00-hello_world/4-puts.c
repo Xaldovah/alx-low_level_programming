dd85f1db962b5b8544cab9f608b71905900e8b76
Xaldovah <dxaldovah@gmail.com>
2023-02-16 12:52:56 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a program that runs puts function
+ * Return: 0 (Let's go!)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
