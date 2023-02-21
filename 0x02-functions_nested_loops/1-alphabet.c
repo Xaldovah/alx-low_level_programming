72d3c39d06ccec0ddc9ebdce604bf33f1a58600f
Xaldovah <dxaldovah@gmail.com>
2023-02-21 01:34:58 -0800

    Insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - print alphabet
+  *
+  * Return: void
+  */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
