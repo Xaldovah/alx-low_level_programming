fd6fb6262ca110c42d849e6d288b66e1924a598c
Xaldovah <dxaldovah@gmail.com>
2023-02-22 16:02:47 -0800

    Pos/neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: 0 (success)
- */
+* main - tests if positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
@@ -12,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	return(0);
+
+	return (0);
 }
