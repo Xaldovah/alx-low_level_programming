3616b219dbfc938e91dcab2075a08cd89c1704c0
Xaldovah <dxaldovah@gmail.com>
2023-02-27 14:51:07 -0800

    Hacker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,54 +10,26 @@
 
 int main(void)
 {
-    char password[84];
-    int index = 0, sum = 0, diff_half1, diff_half2;
+    int pass[100];
+    int i, sum, n;
+
+    sum = 0;
 
     srand(time(NULL));
 
-    while (sum < 2772)
+    for (i = 0; i < 100; i++)
     {
-        password[index] = 33 + rand() % 94;
-        sum += password[index++];
+        pass[i] = rand() % 78;
+        sum += (pass[i] + '0');
+	putchar(pass[i] + '0');
+	if ((2772 - sum) - '0' < 78)
+	{
+		n = 2772 - sum - '0';
+		sum += n;
+		putchar(n + '0');
+		break;
+	}
     }
-
-    password[index] = '\0';
-
-    if (sum != 2772)
-    {
-        diff_half1 = (sum - 2772) / 2;
-        diff_half2 = (sum - 2772) / 2;
-
-        if ((sum - 2772) % 2 != 0)
-        {
-            diff_half1++;
-        }
-
-        for (index = 0; password[index]; index++)
-        {
-            if (password[index] >= (33 + diff_half1))
-            {
-                password[index] -= diff_half1;
-                sum -= diff_half1;
-            }
-        }
-
-        for (index = 0; password[index]; index++)
-        {
-            if (sum == 2772)
-            {
-                break;
-            }
-
-            if (password[index] >= (33 + diff_half2))
-            {
-                password[index] -= diff_half2;
-                sum -= diff_half2;
-            }
-        }
-    }
-
-    printf("%s", password);
 
     return 0;
 }
