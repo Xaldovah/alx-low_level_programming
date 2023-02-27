061d8c31c13507fd79c62cb7c8a082dc320c0a37
Xaldovah <dxaldovah@gmail.com>
2023-02-27 14:28:50 -0800

    Don't hate the hacker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,49 +10,54 @@
 
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+    char password[84];
+    int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srand(time(0));
+    srand(time(NULL));
 
-	while (sum < 2772)
-	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
+    while (sum < 2772)
+    {
+        password[index] = 33 + rand() % 94;
+        sum += password[index++];
+    }
 
-	password[index] = '\0';
+    password[index] = '\0';
 
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
+    if (sum != 2772)
+    {
+        diff_half1 = (sum - 2772) / 2;
+        diff_half2 = (sum - 2772) / 2;
 
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
+        if ((sum - 2772) % 2 != 0)
+        {
+            diff_half1++;
+        }
 
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		
-		for (index = 0; password[index]; index++)
+        for (index = 0; password[index]; index++)
+        {
+            if (password[index] >= (33 + diff_half1))
+            {
+                password[index] -= diff_half1;
+                sum -= diff_half1;
+            }
+        }
 
-		{
-			if (password[index] >= (33 + diff_half2))
+        for (index = 0; password[index]; index++)
+        {
+            if (sum == 2772)
+            {
+                break;
+            }
 
+            if (password[index] >= (33 + diff_half2))
+            {
+                password[index] -= diff_half2;
+                sum -= diff_half2;
+            }
+        }
+    }
 
-				password[index] -= diff_half2;
-			break;
+    printf("%s", password);
 
-		}
-	}
-
-	printf("%s", password);
-
-	return (0);
+    return 0;
 }
