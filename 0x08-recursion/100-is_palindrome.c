6749c950acd9f1ffe06518a0df1d53fc9ec96159
Xaldovah <dxaldovah@gmail.com>
2023-03-08 14:14:27 -0800

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int _strlen_recursion(char *s)
   */
 int comparator(char *s, int n1, int n2)
 {
-	if (*(s n1) == *(s + n2))
+	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
