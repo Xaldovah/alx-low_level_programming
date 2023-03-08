cf993f0a7e6e10bad3461e71cd853f41aecd7d61
Xaldovah <dxaldovah@gmail.com>
2023-03-08 14:26:56 -0800

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,7 +27,7 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return comparator(s, n1 + 1, n2 - 1);
+		return (comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
