eef210298056a2d70efe93bcdaa07a0b5aaed6aa
Dennis Kimani <dxaldovah@gmail.com>
2023-04-03 17:09:46 +0300

    add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,18 +13,26 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_list;
 
+	// Check if the double pointer to the head of the list is not NULL
 	if (head !== NULL)
 	{
+		// Allocate memory for a new node in the list
 		new_list = malloc(sizeof(listint_t));
+		// Check if new list is NULL, if true return NULL
 		if (new_list == NULL)
 			return (NULL);
 
+		// Initialize the new node with the given value and the current head of the list as its next node
 		new_list->n = n;
 		new_list->next = *head;
+
+		// Update the head of the list to point to the new node
 		*head = new_list;
 
+		// Return a pointer to the new node
 		return (new_list);
 	}
 
+	// Return NULL if the double pointer to the head of the list is NULL
 	return (NULL);
 }
