2e0b9c5a3002dfcc99a41d881bdbe8f4685ed3db
Dennis Kimani <dxaldovah@gmail.com>
2023-04-03 17:59:55 +0300

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,13 +13,22 @@ int pop_listint(listint_t **head)
 	listint_t *new_head;
 	int n = 0;
 
+	// Check if the head pointer is not NULL
 	if (*head != NULL)
 	{
+		// Save the next node in the list
 		new_head = (*head)->next;
+
+		// Save the integer value of the current node
 		n = (*head)->n;
+
+		// Free the memory allocated for the current node
 		free(*head);
+
+		// Update the head pointer to the next node in the list
 		*head = new_head;
 	}
 
-	return (0);
+	// Return the value of the removed node
+	return (n);
 }
