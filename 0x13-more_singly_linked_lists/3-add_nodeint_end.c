a619c57ff3575178cb68a4515286c29248517c95
Dennis Kimani <dxaldovah@gmail.com>
2023-04-03 17:24:50 +0300

    add node to the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,22 +14,28 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_list, *temp;
 
+	// Check if the double pointer to the head of the list is not NULL
 	if (head)
 	{
+		// Allocate memory for a new node in the list
 		new_list = malloc(sizeof(listint_t));
 		if (new_list == NULL)
 			return (NULL);
 
+		// Initialize the new node with the given value and NULL as its next node
 		new_list->n = n;
 		new_list->next = NULL;
 
+		// Check if the list is currently empty
 		if (*head == NULL)
 		{
+			// If it is, set the new node as the head of the list and return a pointer to it
 			*head = new_list;
 			return (*head);
 		}
 		else
 		{
+			// If it is not empty, traverse the list until the end and add the new node there
 			temp = *head;
 			while (temp->next)
 				temp = temp->next;
@@ -39,5 +45,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 	}
 
+	// Return NULL if the double pointer to the head of the list is NULL
 	return (NULL);
 }
