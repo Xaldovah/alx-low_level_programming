a08b2449619d99efb4707bd77cba5d987aa8dcb9
Dennis Kimani <dxaldovah@gmail.com>
2023-04-03 17:34:07 +0300

    Free all

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,14 +10,23 @@
 
 void free_listint(listint_t *head)
 {
+	// Declare a temporary pointer to store the current node
 	listint_t *temp;
 
+	// Use a while loop to traverse the list and free each node
 	while (head)
 	{
+		// Save the current node in a temporary pointer
 		temp = head;
+
+		// Update the head pointer to the next node
 		head = head->next;
+		
+		// Free the memory allocated for the current node
 		free(temp);
 	}
 
+	// Free the memory allocated for the head pointer
+	// Even though head is now NULL it is necessary
 	free(head);
 }
