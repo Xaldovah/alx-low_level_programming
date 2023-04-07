178a5bd7b00bad3473c27eb70d3d4ac3a1a65932
Xaldovah <dxaldovah@gmail.com>
2023-04-07 03:48:34 -0700

    latest changes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
 void print_binary(unsigned long int n);
 void _print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
