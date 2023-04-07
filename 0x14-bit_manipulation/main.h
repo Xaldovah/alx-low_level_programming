c5e8be465fb65225c57e925d400ad5752f2a22fe
Xaldovah <dxaldovah@gmail.com>
2023-04-07 12:53:29 -0700

    latest commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int _strlen(const char *s);
 void print_binary(unsigned long int n);
 void _print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
