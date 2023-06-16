c3a75d3ae7d0e5d5d9a635fcf68f518efa90348f
Dennis Kimani <dxaldovah@gmail.com>
2023-06-16 08:51:10 -0700

    Crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,41 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <time.h>
 
 /**
- * main - ...
+ * generate_key - Generate a key
  *
- * Return: 0.
+ * Return: Pointer to generated key (char *)
+ */
+char *generate_key(void)
+{
+	char possible_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char *key = malloc(9 * sizeof(char));
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < 7; i++)
+	{
+		key[i] = possible_chars[rand() % (sizeof(possible_chars) - 1)];
+	}
+	key[7] = '@';
+	key[rand() % 8] = '-';
+	key[8] = '\0';
+
+	return (key);
+}
+
+/**
+ * test_key_gen - Test the key generation
+ * @program: Program name
+ * @test_count: Number of tests
+ */
+void test_key_gen(const char *program, int test_count)
+{
+	int i;
+	char *key, command[50];
+	FILE *pipe;
+
+	for (i = 0; i < test_count; i++)
+	{
+		key = generate_key();
+		printf("test %d: %s\n", i + 1, key);
+		sprintf(command, "%s %s", program, key);
+		pipe = popen(command, "r");
+		pclose(pipe);
+		free(key);
+	}
+	printf("completed %d successful runs!\n", test_count);
+}
+
+/**
+ * main - Entry point
+ * Return: 0
  */
 int main(void)
 {
-	char a[10], c[10];
-	int i, j, k = 0;
-
-	printf("#Keygen by b44nz0r\n\n");
-
-	while (k < 5 || k >= 10)
-	{
-		if (k != 0)
-			printf("\nThe username length should be 5 to 10 alphabets\n");
-		printf("enter username: ");
-		scanf("%s", a);
-		k = strlen(a);
-	}
-	i = k - 1;
-	j = 0;
-
-	while (i >= 0)
-	{
-		c[j] = a[i] + i;
-		i--;
-		j++;
-	}
-	c[j] = 0;
-	printf("\nThe password is %s\n", c);
-	printf("\nHit Enter to Exit\n");
-	getchar();
-	getchar();
+	test_key_gen("./crackme5", 50);
 
 	return (0);
 }
