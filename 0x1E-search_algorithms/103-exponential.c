05cb26976491d09f08e4820ae2f4fecec7ad2e85
xaldovah <dxaldovah@gmail.com>
2023-10-09 22:00:38 +0000

    function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -10,3 +10,68 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
+	int pointer;
+
+	if (size == 0)
+	{
+		return (-1);
+	}
+	pointer = 1;
+	while (pointer < size && array[pointer] < value)
+	{
+		pointer *= 2;
+	}
+	return (binary_search(array, value, pointer/2, min(pointer + 1, size)))
+}
+
+
+/**
+ *  * binary_search - Search for a value in a sorted array using binary search
+ *   * @array: Pointer to the first element of the sorted array
+ *    * @size: Number of elements in the array
+ *     * @value: The value to search for
+ *      *
+ *       * Return: Index where `value` is located, -1 if not found or `array` is NULL.
+ *        */
+int binary_search(int *array, size_t size, int value)
+{
+	int i, left = 0, mid, right = (int)size - 1;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+
+			if (i < right)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf("\n");
+			}
+		}
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	return (-1);
+}
