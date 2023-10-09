974674442d33560ff401c1546084e1085e49d662
Dennis Kimani <dxaldovah@gmail.com>
2023-10-09 15:39:07 -0700

    function that searches for a value in a sorted array of integers using the Exponential search algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -26,13 +26,13 @@ int exponential_search(int *array, size_t size, int value)
 
 
 /**
- *  * binary_search - Search for a value in a sorted array using binary search
- *   * @array: Pointer to the first element of the sorted array
- *    * @size: Number of elements in the array
- *     * @value: The value to search for
- *      *
- *       * Return: Index where `value` is located, -1 if not found or `array` is NULL.
- *        */
+ * binary_search - Search for a value in a sorted array using binary search
+ * @array: Pointer to the first element of the sorted array
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: Index where `value` is located, -1 if not found or `array` is NULL.
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	int i, left = 0, mid, right = (int)size - 1;
